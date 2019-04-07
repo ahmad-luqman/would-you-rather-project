50cf978c65505e4da535c99c0153f71d718fc6b2
Ahmad Luqman <ahmad.luqman@gmail.com>
2019-04-07 13:27:39 +0500

    mark your answer clearly

--- src/components/QuestionResults.js
+++ src/components/QuestionResults.js
@@ -4,7 +4,7 @@ import ProgressBar from './ProgressBar'
 import Nav from './Nav'
 
 const QuestionResults = (props) => {
-  const { id, questions, users } = props
+  const { authedUser, id, questions, users } = props
   const question = questions[id]
   const totalCount = question.optionOne.votes.length + question.optionTwo.votes.length
   const optionOneCount = question.optionOne.votes.length
@@ -14,20 +14,33 @@ const QuestionResults = (props) => {
   const author = question.author
   const authorName = users[author].name
   const avatarURL = users[author].avatarURL
+  const authedUserAnswer = users[authedUser].answers[id]
   return (
     <div>
       <Nav />
-      <div>
+      <div className="question">
         <h2>Asked by {authorName}:</h2>
         <img src={avatarURL}
                   alt='avatar'
                   className='avatar' />
-        <div>Would you rather {question.optionOne.text}?</div>
-        <ProgressBar percentage={optionOnePercentage} />
-        <div>{optionOneCount} out of {totalCount} votes</div>
-        <div>Would you rather {question.optionTwo.text}?</div>
-        <ProgressBar percentage={optionTwoPercentage} />
-        <div>{optionTwoCount} out of {totalCount} votes</div>
+        <div className={`result-owner ${authedUserAnswer === 'optionOne'}`}>
+          <div>Would you rather {question.optionOne.text}?</div>
+          <ProgressBar percentage={optionOnePercentage} />
+          <div>{optionOneCount} out of {totalCount} votes</div>
+          {authedUserAnswer === 'optionOne' &&
+          <div>
+            <small className="white-text">Your Answer</small>
+          </div>}
+        </div>
+        <div className={`result-owner ${authedUserAnswer === 'optionTwo'}`}>
+          <div>Would you rather {question.optionTwo.text}?</div>
+          <ProgressBar percentage={optionTwoPercentage} />
+          <div>{optionTwoCount} out of {totalCount} votes</div>
+          {authedUserAnswer === 'optionTwo' &&
+          <div>
+            <small className="white-text">Your Answer</small>
+          </div>}
+        </div>
       </div>
     </div>
   )
