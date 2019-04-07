cfab4fbe1b5edc0226ec9011a2e8bc7a95f9cbe4
Ahmad Luqman <ahmad.luqman@gmail.com>
2019-04-07 11:28:02 +0500

    refactor component into a stateless component

--- src/components/QuestionResults.js
+++ src/components/QuestionResults.js
@@ -1,40 +1,36 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import ProgressBar from './ProgressBar'
 import Nav from './Nav'
 
-class QuestionResults extends Component {
-  render () {
-    const { id, questions, users } = this.props
-    const question = questions[id]
-    const totalCount = question.optionOne.votes.length + question.optionTwo.votes.length
-    const optionOneCount = question.optionOne.votes.length
-    const optionTwoCount = question.optionTwo.votes.length
-    const optionOnePercentage = optionOneCount / totalCount * 100
-    const optionTwoPercentage = optionTwoCount / totalCount * 100
-
-    const author = question.author
-    const authorName = users[author].name
-    const avatarURL = users[author].avatarURL
-
-    return (
+const QuestionResults = (props) => {
+  const { id, questions, users } = props
+  const question = questions[id]
+  const totalCount = question.optionOne.votes.length + question.optionTwo.votes.length
+  const optionOneCount = question.optionOne.votes.length
+  const optionTwoCount = question.optionTwo.votes.length
+  const optionOnePercentage = optionOneCount / totalCount * 100
+  const optionTwoPercentage = optionTwoCount / totalCount * 100
+  const author = question.author
+  const authorName = users[author].name
+  const avatarURL = users[author].avatarURL
+  return (
+    <div>
+      <Nav />
       <div>
-        <Nav />
-        <div>
-          <h2>Asked by {authorName}:</h2>
-          <img src={avatarURL}
-                    alt='avatar'
-                    className='avatar' />
-          <div>Would you rather {question.optionOne.text}?</div>
-          <ProgressBar percentage={optionOnePercentage} />
-          <div>{optionOneCount} out of {totalCount} votes</div>
-          <div>Would you rather {question.optionTwo.text}?</div>
-          <ProgressBar percentage={optionTwoPercentage} />
-          <div>{optionTwoCount} out of {totalCount} votes</div>
-        </div>
+        <h2>Asked by {authorName}:</h2>
+        <img src={avatarURL}
+                  alt='avatar'
+                  className='avatar' />
+        <div>Would you rather {question.optionOne.text}?</div>
+        <ProgressBar percentage={optionOnePercentage} />
+        <div>{optionOneCount} out of {totalCount} votes</div>
+        <div>Would you rather {question.optionTwo.text}?</div>
+        <ProgressBar percentage={optionTwoPercentage} />
+        <div>{optionTwoCount} out of {totalCount} votes</div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 function mapStateToProps ({ authedUser, questions, users }, props) {
